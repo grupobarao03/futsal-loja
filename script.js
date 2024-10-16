951cf7d0e27f716e67ead912eac1582845b555a3
grupobarao03 <juansoantos57@gmail.com>
2024-10-16 08:52:42 -0300

    element

---
+++ script.js
@@ -0,0 +1,13 @@
+const novatarefa = document.getElementById('novaTarefa');
+const btnAddTarefa = document.getElementById('btnAddTarefa');
+const listaTarefa = document.getElementById('listaTarefa');
+
+btnAddTarefa.addEventListener('click',function(){
+
+   alert('Bem vindo')
+
+
+
+
+
+});
