a547cbdef35f2c37fb93f90190eda32878974d5e
grupobarao03 <juansoantos57@gmail.com>
2024-10-16 11:30:38 -0300

    corecao js

--- script.js
+++ script.js
@@ -2,36 +2,24 @@ const novatarefa = document.getElementById('novaTarefa');
 const btnAddTarefa = document.getElementById('btnAddTarefa');
 const listaTarefa = document.getElementById('listaTarefa');
 
-btnAddTarefa.addEventListener('click',function(){
-   const tarefa = novatarefa.value;
-
-   // se a tarefa for diferente de vazia ele execulta
-   if(tarefa.trim() !== " "){
-
-    const li = document.createComment('li');
-
-   li.textContent = tarefa;
-
-   const botaoRemover = document.createComment('button');
-   botaoRemover.textContent = "Remover";
-
-   botaoRemover.addEventListener('click', function(){
-      li.remove();
-   });
-
-   const div = document.createElement('div');
-    div.appendChild(botaoRemover);
-    li.appendChild(div);
-
-    listaTarefa.appendChild(li);
-
-    
-    novatarefa.value = "";
-
-   }
-
-
-
-
-
+btnAddTarefa.addEventListener('click', function() {
+    const tarefa = novatarefa.value;
+    // se a tarefa for diferente de vazia ele execulta
+    if (tarefa) {
+        const li = document.createElement('li');
+        li.textContent = tarefa;
+        
+        const botaoRemover = document.createElement('button');
+        botaoRemover.textContent = "Remover";
+        botaoRemover.addEventListener('click', function() {
+            li.remove();
+        });
+        
+        const div = document.createElement('div');
+        div.appendChild(botaoRemover);
+        li.appendChild(div);
+        listaTarefa.appendChild(li);
+        
+        novatarefa.value = "";
+    }
 });
